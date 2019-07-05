e6fd0d0599856f17ea7d2ab284ecb7fa67f0898a
Ankur Nigam <ankur.nigam198@gmail.com>
2019-07-06 00:31:52 +0530

    mergeImprove

--- merge.py
+++ merge.py
@@ -1,30 +1,16 @@
 x = [80, 55, 34, 67, 98, 34, 2, 19]
 # x = [5,4,3,2,1]
 
-toMerge = []
-
 
 def splitList(l):
     if len(l) > 1:
-        v = l[:len(l)//2]
-        w = l[len(l)//2:]
-        splitList(v)
-        splitList(w)
-
+        left = l[:len(l)//2]
+        right = l[len(l)//2:]
+        leftSorted = splitList(left)
+        rightSorted = splitList(right)
+        return mergeList(leftSorted, rightSorted)
     else:
-        toMerge.append(l)
-
-
-def merge():
-    while len(toMerge) > 1:
-        fE = toMerge[0]
-        sE = toMerge[1]
-        toMerge.pop(0)
-        toMerge.pop(0)
-
-        # print(toMerge)
-        # print(mergeList(fE,sE))
-        toMerge.append(mergeList(fE, sE))
+        return l
 
 
 def mergeList(a, b):
@@ -42,14 +28,11 @@ def mergeList(a, b):
         elif a[i] > b[j]:
             res.append(b[j])
             j += 1
-        elif a[i]<=b[j]:
+        elif a[i] <= b[j]:
             res.append(a[i])
             i += 1
     return res
 
 
-splitList(x)
-# print(toMerge)
-merge()
-print(toMerge[0])
-print(x)
+y = splitList(x)
+print(y)
